adbb486a50a160fdfc943d9ceb135f746dd67313
Wanessa <wanessa.ant04@gmail.com>
2023-09-27 18:31:25 -0300

    commit add

--- organizar-ativos/src/OrganizadorAtivos.java
+++ organizar-ativos/src/OrganizadorAtivos.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class OrganizadorAtivos {
 
     public static void main(String[] args) {
-
         try (Scanner scanner = new Scanner(System.in)) {
             ArrayList<String> ativos = new ArrayList<>();
 
-            // Entrada dos tipos de ativos
+            // Solicita ao usuário que insira a quantidade de ativos
+            System.out.print("Digite a quantidade de ativos: ");
             int quantidadeAtivos = scanner.nextInt();
             scanner.nextLine(); // Consumir a quebra de linha pendente
 
-            // Entrada dos códigos dos ativos
+            // Solicita ao usuário que insira os tipos de ativos
+            System.out.println("Digite os tipos de ativos:");
+
             for (int i = 0; i < quantidadeAtivos; i++) {
                 String codigoAtivo = scanner.nextLine();
                 ativos.add(codigoAtivo);
@@ -23,10 +25,12 @@ public class OrganizadorAtivos {
             Collections.sort(ativos);
 
             // Imprimir a lista de ativos ordenada
+            System.out.println("Ativos ordenados:");
             for (String ativo : ativos) {
                 System.out.println(ativo);
             }
         }
     }
 }
+
 
